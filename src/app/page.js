c6661628ec3316bcf382227b460d2695fcf3c927
Gourav Makode <140704003+GT0SRT@users.noreply.github.com>
2025-01-04 19:16:49 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Nav from "@/components/Nav";
 import Menu from "@/components/Menu";
 import Mainpage from "@/components/Mainpage";
@@ -10,21 +10,27 @@ export default function Home() {
   const isClicked = useSelector((state) => state.update_redux_slice.isClicked);
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      console.log("Component is rendered on the client side");
+    }
+  }, []);
+
   return (
     <div className={`${isClicked ? 'overflow-hidden relative w-full h-screen' : 'relative w-full h-screen'}`}>
       {isClicked && (
         <div className="absolute inset-0 bg-black opacity-50 z-10"></div>
       )}
       <div className={`${isClicked ? 'absolute' : 'absolute'}`}>
-      <Nav />
-      <div className="flex">
-        <Menu />
-        <Mainpage />
+        <Nav />
+        <div className="flex">
+          <Menu />
+          <Mainpage />
+        </div>
       </div>
-      </div>
-      <div className={`${isClicked ? 'absolute z-20':'hidden'}`}>
+      <div className={`${isClicked ? 'absolute z-20' : 'hidden'}`}>
         <Card />
       </div>
     </div>
   );
-}
+};
